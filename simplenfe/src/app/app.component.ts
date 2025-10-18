df20bc247285ce92dcf2c317f15cbad8f458306e
Jpprignolato <joseprignolato@gmail.com>
2025-10-18 14:54:28 -0300

    R

--- simplenfe/src/app/app.component.ts
+++ simplenfe/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { InitialPageComponent } from './initial-page/initial-page.component';
+import { InitialPageComponent } from './pages/initial-page/initial-page.component';
 
 @Component({
   selector: 'app-root',
